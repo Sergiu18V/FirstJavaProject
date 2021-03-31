527bff5db52d55c5223a61168f0db0bf7562dda2
Sergiu18V <sergiucoding@gmail.com>
2021-03-31 21:16:49 +0300

    Printed Hello and name Printed an addition Printed a division Printer the results of mathematical operations

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
-
+// 1. Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+        // Print Hello and my name
+	System.out.println("Hello");
+	System.out.println("Sergiu");
+	// Print an addition
+	System.out.println(3 + 2);
+	// Print a division
+	System.out.println(7/2f);
+	// Print the following mathematical operations
+    System.out.println(-5 + 8 * 6);
+    System.out.println((55+9) % 9 );
+    System.out.println(20 + -3*5 / 8f);
+    System.out.println(5 + 15 / 3 * 2 - 8 % 3);
     }
+
 }
+
