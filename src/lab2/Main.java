74d55bcd97d166d4a7307661fc651d2c45dbd59e
Sergiu18V <sergiucoding@gmail.com>
2021-04-03 17:12:39 +0300

    Simplified the code

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -2,41 +2,33 @@ package lab2;
 
 public class Main {
 
-//    static int mulNum1 = 8;
-//    static int mulNum2 = 4;
-
     public static void main(String[] args) {
         // Multiply
-//        int multiply1 = multiply(mulNum1, mulNum2);
-        int multiply = multiply(6, 3);
-//        int divide1 = division(num1, num2);
-//        int divide2 = division(4, 2);
-        System.out.println(multiply);
-//        System.out.print(multiply(mulNum1, mulNum2));
-//        System.out.println(divide1);
-//        System.out.print(division(num1, num2));
+        System.out.println("The result of the multiplication is: " + multiply(7, 8));
+
         // Division
-        double divide = division(34, 5);
-        System.out.println(divide);
+        System.out.println("The result of the division is: " + division(4, 2));
 
         // Modulo
-        int moduloOperation = remainder(6, 3);
-        System.out.println(moduloOperation);
+        System.out.println("The remainder is: " + remainder(8, 4));
     }
 
     public static int multiply(int first, int second) {
-        int multiply = first * second;
-        return multiply;
+//        int multiplyResult = first * second;
+        return first * second;
+//        return multiplyResult;
     }
 
-    public static int division(int first, int second) {
-        int division = first / second;
-        return division;
+    public static float division(float first, int second) {
+//        float divisionResult = first / second;
+        return first / second;
+//        return divisionResult;
     }
 
     public static int remainder(int first, int second) {
-        int remainder = (first % second);
-        return remainder;
+//        int remainderResult = (first % second);
+        return first % second;
+//        return remainderResult;
     }
 
 }
