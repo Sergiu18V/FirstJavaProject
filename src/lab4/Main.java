4d4b8c2e149d7f8efdac741b31f22024177b3299
Sergiu18V <sergiucoding@gmail.com>
2021-04-10 21:10:15 +0300

    For loops

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -3,11 +3,11 @@ package lab4;
 public class Main {
     public static void main(String[] args) {
         LogicalOp op = new LogicalOp();
-//        op.printToHundred(1);
-//        op.printFromHundred(100);
-//        op.fromOneToTwo(5, 60);
-//        op.numarPar(1);
-//        op.numarImpar(1);
+        op.printToHundred(1);
+        op.printFromHundred(100);
+        op.fromOneToTwo(5, 60);
+        op.numarPar(1);
+        op.numarImpar(1);
         System.out.println("The sum of the numbers is: " + op.total(1));
         System.out.println("The average of the numbers is: " + op.average(1));
         op.stairs(7);
